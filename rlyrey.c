d34ce8401b2c10b759f503ff149829769eaab9c4
RAHULSONI1998 <59302983+RAHULSONI1998@users.noreply.github.com>
2019-12-28 14:20:22 +0530

    Add files via upload

---
+++ rlyrey.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "conio2.h"
+#include "rlyres.h"
+
+int enterchoice()
+{
+
+    int choice ,i;
+
+    textcolor(YELLOW);
+    gotoxy(28,1);
+    printf(" RAILWAY RESERVATION SYSTEM \n");
+
+    for(i=1;i<=73;i++)
+        printf("-");
+
+
+}
